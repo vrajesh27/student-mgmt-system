5f1953a73bbb2b596252bbbd35529548beb46cb1
Venkatesan,Rajesh <Rajesh.Venkatesan@Cerner.com>
2018-06-09 11:23:22 +0530

    Simple MVC Application created using dotnet new mvc --auth None

---
+++ SMServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SMServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
